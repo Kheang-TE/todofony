10c5ec2288f9ec803f8ab1e8f55183427f23b596
Kheang TE <dev.kheang.te@gmail.com>
2026-02-21 16:35:42 +0100

    feat: login with jwt + cookie and logout

--- src/Controller/API/AuthController.php
+++ src/Controller/API/AuthController.php
@@ -2,49 +2,48 @@
 
 namespace App\Controller\API;
 
-use App\DTO\RegisterUserDTO;
+use App\DTO\AuthDTO;
 use App\Service\AuthService;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api', name:'api_')]
 final class AuthController extends AbstractController
 {
-    #[Route('/register', name: 'register', methods:['POST'])]
-    public function register(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, AuthService $authService): JsonResponse
+    #[Route('/register', name:'register', methods:['POST'])]
+    public function register(Request $request, SerializerInterface $serializer, AuthService $authService): JsonResponse
     {
         try{
 
             // Deserialize JSON to DTO
-            $dto = $serializer->deserialize($request->getContent(), RegisterUserDTO::class, 'json');
+            $dto = $serializer->deserialize($request->getContent(), AuthDTO::class, 'json');
+
+            $existingUser = $authService->existingUser($dto);
+            if($existingUser){
+                return $this->json([
+                    'error' => 'Email already exists'
+                ], Response::HTTP_CONFLICT); // 409 Conflict
+            }
 
         } catch(\Exception $e){
 
             return $this->json([
                 'error' => 'Invalid JSON format'
-            ], Response::HTTP_BAD_REQUEST);
+            ], Response::HTTP_BAD_REQUEST); // 400 Bad Request
 
         }
 
-        // Datas DTO Validation 
-        $errors = $validator->validate($dto);
-
-        if(count($errors) > 0){
-
-            $formattedErrors = [];
-
-            foreach($errors as $error){
-                $formattedErrors[$error->getPropertyPath()] = $error->getMessage();
-            }
-
+        // Datas DTO Validation
+        $errors = $authService->validationUser($dto);
+        if($errors){
             return $this->json([
-                'errors' => $formattedErrors
-            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+                'errors' => $errors
+            ], Response::HTTP_UNPROCESSABLE_ENTITY); // 422 Unprocessable Entity
         }
 
         // Use Authservice->registerUser for save new user
@@ -57,6 +56,31 @@ final class AuthController extends AbstractController
                 'email' => $user->getEmail(),
                 'role' => $user->getRoles()
             ]
-        ], Response::HTTP_CREATED);
+        ], Response::HTTP_CREATED); // 201 Created
     }
+
+    // The login route is handled by the security firewall, so we just return an error message here.
+    #[Route('/login', name:'login', methods:['POST'])]
+    public function login(Request $request, SerializerInterface $serializer, AuthService $authService): JsonResponse{
+
+        // 
+        return $this->json([
+            'error' => 'Verify the firewall configuration. The login path is not properly defined.'
+        ], Response::HTTP_UNAUTHORIZED); // 401 Unauthorized
+
+    }
+
+    #[Route('/logout', name:'logout', methods:['POST'])]
+    public function logout(): JsonResponse
+    {
+        $response = new JsonResponse([
+            'message' => 'Successfully logged out'
+        ], Response::HTTP_OK); // 200 OK
+
+        // Clear the authentication cookie
+        $response->headers->clearCookie('BEARER');
+
+        return $response;
+    }
+
 }
--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -2,24 +2,47 @@
 
 namespace App\Service;
 
-use App\DTO\RegisterUserDTO;
+use App\DTO\AuthDTO;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AuthService{
 
     public function __construct(
+        private ValidatorInterface $validator,
+        private UserRepository $userRepository,
         private UserPasswordHasherInterface $hasher,
+        private JWTTokenManagerInterface $jwtManager,
         private EntityManagerInterface $emi
     ){}
 
-    public function registerUser(RegisterUserDTO $dto):User{
+    public function validationUser(AuthDTO $dto): array{
+        $errors = $this->validator->validate($dto);
+
+        $formattedErrors = [];
+        
+        if(count($errors) > 0){
+            foreach($errors as $error){
+                $formattedErrors[$error->getPropertyPath()] = $error->getMessage();
+            }
+        }
+
+        return $formattedErrors;
+    }
+
+    public function existingUser(AuthDTO $dto): ?User{
+        return $this->userRepository->findOneBy(['email' => strtolower(trim($dto->email))]);
+    }
+
+    public function registerUser(AuthDTO $dto):User{
 
         $user = new User();
         
-        $user->setEmail($dto->email);
+        $user->setEmail(strtolower(trim($dto->email)));
         
         $hashPwd = $this->hasher->hashPassword($user, $dto->password);
         $user->setPassword($hashPwd);
