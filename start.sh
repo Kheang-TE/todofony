b88e22f5364268f099e56be555ff531b28fbc51b
Kheang TE <dev.kheang.te@gmail.com>
2026-02-25 23:43:32 +0100

    new setting for prod

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "Nettoyage du cache..."
+php bin/console cache:clear --env=prod
 
 echo "Installation des dépendances..."
 composer install --no-dev --optimize-autoloader
 
-echo "Création de la base de données et exécution des migrations..."
-php bin/console doctrine:migrations:migrate
+echo "Création de la base de données..."
+php bin/console doctrine:database:create
 
-echo "Nettoyage du cache..."
-php bin/console cache:clear --env=prod
+echo "Exécution des migrations..."
+php bin/console doctrine:migrations:migrate
 
 echo "Démarrage du serveur..."
 php -S 0.0.0.0:$PORT -t public
